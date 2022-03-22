4d6573718c229a0c26a450f84d0abd39a7b53909
kazuma-inazaki <kazuma.inazaki@connec10.co.jp>
2022-03-22 14:34:14 +0900

    Initial Commit

---
+++ connect-notifications-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  connect-notifications-app
+//
+//  Created by Kazuma Inazaki on 2022/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
